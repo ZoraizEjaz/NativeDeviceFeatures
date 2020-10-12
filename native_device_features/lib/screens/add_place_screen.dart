d0b0b5db940e271a656274af9d582d5988c4c2de
zoraizBitBucket <zoraiz93@gmail.com>
2020-10-12 11:38:03 +0500

    Location dependecies and design added

--- native_device_features/lib/screens/add_place_screen.dart
+++ native_device_features/lib/screens/add_place_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import '../widgets/image_input.dart';
 import 'package:provider/provider.dart';
 import '../providers/great_places.dart';
+import '../widgets/location_input.dart';
 
 class AddPlaceScreen extends StatefulWidget {
   static const routeName = '/add-place';
@@ -56,6 +57,10 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
                       width: 10,
                     ),
                     ImageInput(_selectImage),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    LocationInput(),
                   ],
                 ),
               ),
---
+++ native_device_features/lib/widgets/location_input.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class LocationInput extends StatefulWidget {
+  @override
+  _LocationInputState createState() => _LocationInputState();
+}
+
+class _LocationInputState extends State<LocationInput> {
+  String _previewImageUrl;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          height: 170,
+          width: double.infinity,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 1,
+              color: Colors.grey,
+            ),
+          ),
+          child: _previewImageUrl == null
+              ? Text('No Location Choosen')
+              : Image.network(
+                  _previewImageUrl,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FlatButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.map),
+              textColor: Theme.of(context).accentColor,
+              label: Text('Current Location'),
+            ),
+            FlatButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.map),
+              textColor: Theme.of(context).accentColor,
+              label: Text('Select on Map'),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
