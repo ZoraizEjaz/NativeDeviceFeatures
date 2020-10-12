d890ad56c4ea27c2da85e98a4a3928a2e303482f
zoraizBitBucket <zoraiz93@gmail.com>
2020-10-12 10:49:25 +0500

    Storing and fetching data with sqflite

--- native_device_features/lib/providers/great_places.dart
+++ native_device_features/lib/providers/great_places.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:flutter/foundation.dart';
 import '../models/place.dart';
+import '../helpers/db_helper.dart';
 
 class GreatPlaces with ChangeNotifier {
   List<Place> _items = [];
@@ -23,5 +24,28 @@ class GreatPlaces with ChangeNotifier {
 
     _items.add(newPlace);
     notifyListeners();
+    DBHelper.insert('places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.address,
+    });
+  }
+
+  Future<void> fetchAndSetPlaces() async {
+    final dataList = DBHelper.getData('places');
+
+    dataList.then((list) {
+      _items = list
+          .map(
+            (item) => Place(
+              id: item['id'],
+              title: item['title'],
+              image: File(item['image']),
+              address: null,
+            ),
+          )
+          .toList();
+      notifyListeners();
+    });
   }
 }
