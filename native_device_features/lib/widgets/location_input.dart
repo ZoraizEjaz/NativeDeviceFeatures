d919c3fe68e324274bcc84c02b50443ebb4b4306
zoraizBitBucket <zoraiz93@gmail.com>
2020-10-13 13:14:53 +0500

    Allowing user to select a location and return the selected location on pop of a screen

--- native_device_features/lib/widgets/location_input.dart
+++ native_device_features/lib/widgets/location_input.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 import '../helpers/location_helper.dart';
 import '../screens/map_screen.dart';
@@ -22,7 +23,8 @@ class _LocationInputState extends State<LocationInput> {
   }
 
   Future<void> _selectOnMap() async {
-    final selectedLocation = await Navigator.of(context).push(
+    // here we will get LntLng when screen gets pop
+    final LatLng selectedLocation = await Navigator.of(context).push<LatLng>(
       MaterialPageRoute(
           fullscreenDialog: true,
           builder: (ctx) => MapScreen(
@@ -33,6 +35,7 @@ class _LocationInputState extends State<LocationInput> {
     if (selectedLocation == null) {
       return;
     }
+    print(selectedLocation.latitude);
   }
 
   @override
